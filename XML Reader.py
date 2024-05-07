dfcf4a0102ae5965c6a44acc67d1c7fe96b96c0d
Khalil <reza21khalil@gmail.com>
2024-05-07 09:50:56 -0700

    second commit

--- XML Reader.py
+++ XML Reader.py
@@ -1,12 +1,13 @@
-#Import ElementTree 
+#Imports ElementTree module from library
 import xml.etree.ElementTree as ET
 
-#Define XML file
+#Define and assign XML file (holds the path of the file we want it to read)
 xml_file = "15Seconds_TulumNoPower_200Count.xml"
 
-#Function to read the XML file and create a txt file
+#Function to read the XML file and create a txt file while extracting the tags
 def extract_and_write_tags(xml_file):
-    #Parse the XML file
+    #Parse the XML file --> stored in the 'tree' variable. 
+    #Use the 'getroot' method to obtain root element from the xml
     tree = ET.parse(xml_file)
     root = tree.getroot()
 
@@ -14,10 +15,12 @@ def extract_and_write_tags(xml_file):
     tags = []
 
     #Extract tags from XML
+    #Loop iterates over all elements and retrieves the tag name using 'tag'
     for elem in root.iter():
         tags.append(elem.tag)
 
     #Write tags to newly created text file
+    #Loop through each tag and write it to the text file 
     with open("tags.txt", "w") as file:
         for tag in tags:
             file.write(tag + "\n")
@@ -25,5 +28,5 @@ def extract_and_write_tags(xml_file):
     #Confirm the process completion
     print("Tags have been successfully written to tags.txt.")
 
-#Execute the function
+#Execute the function and pass it to the xml file
 extract_and_write_tags(xml_file)
